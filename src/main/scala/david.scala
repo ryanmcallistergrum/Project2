81a5d5a7cc5ec0ec14049c9549abe003764f3927
Jaime Hinojos <jaimehinojos11@gmail.com>
2022-01-06 15:58:03 -0700

    Added country_stats files, also added methos country and by date

--- src/main/scala/david.scala
+++ src/main/scala/david.scala
@@ -1,6 +1,16 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, to_date}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import org.apache.spark.sql.catalyst.dsl.expressions.{DslExpression, StringToAttributeConversionHelper, windowSpec}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{abs, coalesce, col, count, date_format, lag, lit, round, to_date, when}
+import org.apache.spark.sql.types.{DateType, IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.Row
+import java.io.File
+
+
 
 object david {
   def main(args:Array[String]): Unit ={
@@ -23,6 +33,7 @@ object david {
 
     val df = spark.read.option("header",true)
       .csv("data/covid_19_data_cleaned.csv").na.fill(0).na.fill("")
+    organize_by_country(df, spark)
 
     val cont = spark.read.option("header", true)
       .csv("data/continents.csv")
@@ -104,4 +115,79 @@ object david {
 
 
   }
+  def organize_by_country(df:DataFrame, spark:SparkSession): Unit ={
+    val file = new File("data/country_stats_monthly/")
+    if (file.exists() && file.isDirectory){
+      val list = file.listFiles.filter(_.isFile).filter(_.getName.endsWith(".csv")).toList
+      list.foreach(l => {
+        val country_data_month = spark.read.option("header", true)
+          .csv(l.toString).na.fill(0).na.fill("")
+        country_data_month.withColumnRenamed("sum(Deaths)", "Deaths").withColumnRenamed("sum(Confirmed)", "Confirmed").
+          withColumnRenamed("sum(Recovered)", "Recovered").show()
+
+
+      })
+
+
+
+    }else{
+      var n_df = df.withColumn("ObservationDate", to_date(col("ObservationDate"),"MM/dd/yyyy"))
+      n_df = n_df.withColumn("ObservationDate", date_format(col("ObservationDate"),"yyyy-MM"))
+      n_df = n_df.withColumn("Confirmed",col("Confirmed").cast("int")).withColumn(
+        "Deaths",col("Deaths").cast("int")
+      ).withColumn(
+        "Recovered",col("Recovered").cast("int")
+      )
+      val countries = n_df.select("`Country/Region`").distinct().collect()
+      val country_stats = countries.map(row => {
+        var country = n_df.where(s"`Country/Region` == '${row.get(0)}'").groupBy("`Province/State`", "ObservationDate").
+          max("Confirmed", "Deaths", "Recovered").orderBy("ObservationDate")
+        country.withColumnRenamed("max(Deaths)", "Deaths").withColumnRenamed("max(Confirmed)", "Confirmed").
+          withColumnRenamed("max(Recovered)", "Recovered").groupBy("ObservationDate").sum("Confirmed", "Deaths", "Recovered").withColumn("Country", lit(row.get(0)) )
+      }).reduce((a: DataFrame, b: DataFrame) => a.union(b))
+      country_stats.coalesce(1).write.format("com.databricks.spark.csv")
+        .option("header", "true")
+        .save("data/country_stats_monthly.csv")
+    }
+    //spark.sql("SELECT * from Country").show()
+
+
+
+
+  }
+
+
+  def organize_by_month(df:DataFrame ): Unit ={
+    var months = df.select( col("SNo"),to_date(col("ObservationDate"),"MM/dd/yyyy").alias("Date"))
+    months = df.join(months,df("SNo") === months("SNo"),"inner" ).select(df("SNo"), col("Date"),
+      col("`Province/State`").alias("Province"), col("`Country/Region`").alias("Country"), col("Confirmed"),
+      col("Deaths"), col("Recovered"))
+    months = months.withColumn("Confirmed",col("Confirmed").cast("int")).withColumn(
+      "Deaths",col("Deaths").cast("int")
+    ).withColumn(
+      "Recovered",col("Recovered").cast("int")
+    )
+    months = months.groupBy("Date").sum("Confirmed", "Deaths", "Recovered").orderBy(col("Date").asc).withColumn("Mortality Rate",
+      round(col("sum(Deaths)")/col("sum(Confirmed)"), 3)).withColumnRenamed("sum(Deaths)", "Deaths").
+      withColumnRenamed("sum(Confirmed)", "Confirmed").withColumnRenamed("sum(Recovered)", "Recovered")
+    months = months.select( date_format(col("Date"),"yyyy-MM").alias("Date"), col("Confirmed"), col("Deaths"), col("Recovered"))
+    months = months.groupBy("Date").max("Confirmed", "Deaths", "Recovered").orderBy("Date").withColumnRenamed("max(Deaths)", "Deaths").
+      withColumnRenamed("max(Confirmed)", "Confirmed").withColumnRenamed("max(Recovered)", "Recovered")
+
+    months = months.withColumn("Mortality Rate", round(col("Deaths")/col("Confirmed"), 3))
+    val windowSpec = Window.partitionBy("Date").orderBy(col("Date").asc)
+    months = months.withColumn("Spread Rate",round( (col("Confirmed")-lag("Confirmed", 1).over(Window.partitionBy().
+      orderBy("Date")))/ lag("Confirmed", 1).over(Window.partitionBy().
+      orderBy("Date")), 3 )).withColumn("Difference", coalesce(col("Confirmed")- lag("Confirmed", 1).over(Window.partitionBy().
+      orderBy("Date")), col("Confirmed"))).na.fill(0)
+    months.withColumn("Increase in Cases", round( (col("Difference"))/ lag("Difference", 1).over(Window.partitionBy().
+      orderBy("Date")), 3 ) ).na.fill(0).show()
+
+
+
+
+
+
+  }
+
 }
